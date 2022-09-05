d03f22df6b121b15e12527b3d236f92f2549fc56
shish-ko <andrey.shishko@gmail.com>
2022-09-05 22:02:25 +0300

    added: Our friends slider + popUp

---
+++ index.js
@@ -0,0 +1,66 @@
+import pets from "./data/ourPets.js";
+const track=document.querySelector('.track');
+for(let item in pets){
+    const div=document.createElement("div");
+    div.classList.add('sliderItem')
+    div.innerHTML=`<img style='width: 100%' src="${pets[item].img}" alt="${pets[item].name}">
+    <div class="type">${pets[item].name}</div>
+    <button class='petButton' id="${item}">Learn more</button>`;
+    track.append(div);
+}
+const sliderItem=document.querySelector('.sliderItem')
+const pxToScroll=sliderItem.clientWidth+((track.clientWidth-3*sliderItem.clientWidth)/2);
+console.log(pxToScroll);
+
+const nextButton=document.querySelector('.slideRight');
+const previousButton=document.querySelector('.slideLeft');
+let currentSliderPosition=-3*pxToScroll;
+
+slideNext();
+
+const petButtons=document.querySelectorAll('.petButton');
+const petPopUp=document.querySelector('.petPopUp');
+petButtons.forEach(item=>{
+   
+    item.addEventListener('click', (event)=>{
+         console.log(event.target.id)
+         document.querySelector('.grayBG').classList.add('visible')
+        petPopUp.innerHTML=`    <img src="${pets[event.target.id].img}" alt="${pets[event.target.id].name}" class="petPopupImg">
+        <div class="petInfo">
+            <div class="petName">${pets[event.target.id].name}</div>
+           <h4>${pets[event.target.id].kind}</h4>
+            <div class="petDescription">${pets[event.target.id].description}</div>
+        </div> `
+    });
+})
+
+function buttonListener(){
+    document.querySelectorAll('.roundButton').forEach(item=>item.classList.remove('disableButton'));
+    if(currentSliderPosition===0){
+        previousButton.classList.add('disableButton');
+        previousButton.removeEventListener('click', slidePrev);
+    } else {
+        previousButton.addEventListener('click', slidePrev)
+    }
+    if(currentSliderPosition===-pxToScroll*5){
+        nextButton.classList.add('disableButton');
+        nextButton.removeEventListener('click', slideNext)
+    } else{
+        nextButton.addEventListener('click', slideNext);
+    }
+}
+
+function slideNext(){  
+    currentSliderPosition=currentSliderPosition-pxToScroll;      
+    track.style.transform = `translateX(${currentSliderPosition}px)`;
+    buttonListener()
+}
+function slidePrev(){    
+    currentSliderPosition=currentSliderPosition+pxToScroll;    
+    track.style.transform = `translateX(${currentSliderPosition}px)`;
+    buttonListener()
+}
+
+
+
+// document.getElementById('body').addEventListener('click', (event)=>console.log(event.target))
