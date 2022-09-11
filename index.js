4d59a0765fd2cec96f3f5f3e730a24e0b4496542
shish-ko <andrey.shishko@gmail.com>
2022-09-11 22:13:42 +0300

    corusel instead of slider

--- index.js
+++ index.js
@@ -1,90 +1,124 @@
 import pets from "./data/ourPets.js";
 const track=document.querySelector('.track');
-let currentSliderPosition=0;
-let shownSlide=0
-window.addEventListener('resize', starter);
-function starter(){        
-    pxToScroll=sliderItem.clientWidth+getGapWidth();
-    currentSliderPosition=shownSlide * -pxToScroll;
-    track.style.transform = `translateX(${currentSliderPosition}px)`;
-    buttonListener();
-}
+const startSliderItems=[];
+const nextSliderItems=[];
 
+fillSliderItems(startSliderItems);
+fillSliderItems(nextSliderItems);
 
-for(let item in pets){        
-    const div=document.createElement("div");
-    div.classList.add('sliderItem');
-    div.innerHTML=`<img style='width: 100%' src="${pets[item].img}" alt="${pets[item].name}">
-    <div class="type">${pets[item].name}</div>
-    <button class='petButton' id="${item}">Learn more</button>`;
-    track.append(div);
-}
-
-
-const sliderItem=document.querySelector('.sliderItem');
-function getGapWidth(){
-    const vpWidth=window.getComputedStyle(document.getElementById('body')).width;
-    if (parseInt(vpWidth) >= 1280){
-        return (track.clientWidth-3*sliderItem.clientWidth)/2;
-    } else if(parseInt(vpWidth) < 1280 && parseInt(vpWidth) >= 768){
-        return track.clientWidth-2*sliderItem.clientWidth;
+function fillSliderItems(arr){ 
+    const i = pets[Math.floor(Math.random() * pets.length)];
+    if(startSliderItems.includes(i) || nextSliderItems.includes(i)){
+        fillSliderItems(arr)
     } else {
-        return 0;
+        arr.push(i)
+    }    
+    if(arr.length<itemsPerSlide()){
+        fillSliderItems(arr)  
     }
 }
-let pxToScroll=sliderItem.clientWidth+getGapWidth();
-console.log(sliderItem.clientWidth)
-const nextButton=document.querySelector('.slideRight');
-const previousButton=document.querySelector('.slideLeft');
+
+
+function createDiv(arr){
+    const slideDiv=document.createElement("div");
+    slideDiv.classList.add('slideDiv');
+    for(let item in startSliderItems){       
+        const div=document.createElement("div");
+        div.classList.add('sliderItem');
+        div.innerHTML=`<img style='width: 100%' src="${arr[item].img}" alt="${arr[item].name}">
+        <div class="type">${arr[item].name}</div>
+        <button class='petButton' id="${pets.indexOf(startSliderItems[item])}">Learn more</button>`;
+        slideDiv.append(div);
+    }
+track.append(slideDiv);
+
+}
+
+function restart(){
+    while(track.firstChild){
+        track.removeChild(track.firstChild);
+    }
+    createDiv(nextSliderItems);
+    createDiv(startSliderItems);
+    createDiv(nextSliderItems);
+    pxToScroll=document.querySelector('.slideDiv').clientWidth
+    track.style.transform = `translateX(${-pxToScroll}px)`  
+    const petButtons=document.querySelectorAll('.petButton');
+    petButtons.forEach(item=>{
+   
+        item.addEventListener('click', (event)=>{
+            popUp.classList.add('visible');
+            document.querySelector('.grayBG').classList.add('visible');
+            petPopUp.innerHTML=`    <img src="${pets[event.target.id].img}" alt="${pets[event.target.id].name}" class="petPopupImg">
+            <div class="petInfo">
+                <div class="petName">${pets[event.target.id].name}</div>
+               <h4>${pets[event.target.id].kind}</h4>
+                <div class="petDescription">${pets[event.target.id].description}</div>
+            </div> `
+        });
+    })
+}
+restart();
+var pxToScroll=document.querySelector('.slideDiv').clientWidth;
 
 
 
-const petButtons=document.querySelectorAll('.petButton');
+window.addEventListener('resize', restart);
+
+
+function itemsPerSlide (){
+    const vpWidth=window.getComputedStyle(document.getElementById('body')).width;
+    if (parseInt(vpWidth) >=1280){
+        return 3;
+    } else if(parseInt(vpWidth) < 1280 && parseInt(vpWidth) >= 768){
+        return 2;
+    } else {    
+        return 1;
+    }
+}
+
+
 const petPopUp=document.querySelector('.petPopUp');
 const popUp=document.querySelector('.popUp');
 document.querySelector('.closeButton').addEventListener('click', ()=>{popUp.classList.remove('visible'), document.querySelector('.grayBG').classList.remove('visible')})
-petButtons.forEach(item=>{
-   
-    item.addEventListener('click', (event)=>{
-        popUp.classList.add('visible');
-        document.querySelector('.grayBG').classList.add('visible')
-        petPopUp.innerHTML=`    <img src="${pets[event.target.id].img}" alt="${pets[event.target.id].name}" class="petPopupImg">
-        <div class="petInfo">
-            <div class="petName">${pets[event.target.id].name}</div>
-           <h4>${pets[event.target.id].kind}</h4>
-            <div class="petDescription">${pets[event.target.id].description}</div>
-        </div> `
-    });
-})
 
-function buttonListener(){
-    document.querySelectorAll('.roundButton').forEach(item=>item.classList.remove('disableButton'));
-    if(currentSliderPosition===0){
-        previousButton.classList.add('disableButton');
-        previousButton.removeEventListener('click', slidePrev);
-    } else {
-        previousButton.addEventListener('click', slidePrev)
-    }
-    if(currentSliderPosition===-pxToScroll*(8-Math.floor(track.clientWidth/sliderItem.clientWidth))){
-        nextButton.classList.add('disableButton');
-        nextButton.removeEventListener('click', slideNext)
-    } else{
-        nextButton.addEventListener('click', slideNext);
-    }
-}
-buttonListener();
 
-function slideNext(){  
-    currentSliderPosition=currentSliderPosition-pxToScroll;      
-    track.style.transform = `translateX(${currentSliderPosition}px)`;
-    buttonListener();
-    shownSlide+=1;
+const nextButton=document.querySelector('.slideRight');
+const previousButton=document.querySelector('.slideLeft');
+previousButton.addEventListener('click', slidePrev);
+nextButton.addEventListener('click', slideNext);
+
+
+function slideNext(){   
+    track.classList.add('moveNext');
+    const vpWidth=window.getComputedStyle(document.querySelector('.slideDiv')).width;
+    console.log(vpWidth)
+    console.log(pxToScroll)
+    track.style.transform = `translateX(${-2*pxToScroll}px)` 
+    startSliderItems.length=0;
+    startSliderItems.push(...nextSliderItems);
+    nextSliderItems.length=0;
+    fillSliderItems(nextSliderItems);
+    setTimeout(() => {
+        track.classList.remove('moveNext')
+        restart()
+    }, 1000);
 }
 function slidePrev(){    
-    currentSliderPosition=currentSliderPosition+pxToScroll;    
-    track.style.transform = `translateX(${currentSliderPosition}px)`;
-    buttonListener();
-    shownSlide-=1;
+    track.classList.add('moveNext');
+    track.style.transform = `translateX(0px)` 
+    startSliderItems.length=0;
+    startSliderItems.push(...nextSliderItems);
+    nextSliderItems.length=0;
+    fillSliderItems(nextSliderItems);
+
+    setTimeout(() => {
+        track.classList.remove('moveNext')
+        restart()
+    }, 1000);
+
+    
+
 }
 
 
